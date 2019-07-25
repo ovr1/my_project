7639c8dff679fa912c4607c49205f237ebee6a1c
Lenovo <rybynok@mail.ru>
2019-07-25 13:14:59 +0300

    test

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import find_packages, setup
 
+def read_requirements_txt():
+    with open("requirements.txt") as fp:
+        requirements = [line.strip() for line in fp]
+    return requirements
+
 setup(
     name="mycalc",
     version="0.0.1",
     description="A small example package with some arithmetics",
     packages=find_packages(),
+    install_requires=read_requirements_txt(),
     include_package_data=True,
     author="oleg",
     author_email="rybynok@mail.ru",
